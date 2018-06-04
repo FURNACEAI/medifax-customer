6d35febe0c042952027967a6313a1cabd113f4c7
Bryan Richard <bryan@furnaceai.com>
2018-06-04 16:51:44 -0600

    Updated config file for the production server

--- app/config.py
+++ app/config.py
@@ -2,9 +2,9 @@ import os
 
 _AWS = {
     'headers': {'user-agent': 'medifax/0.0.1', "Content-Type":"application/json" },
-    'status': 'dev',
+    'status': 'prod',
     'employees': {
-        'base' : 'https://3ts6m0h20j.execute-api.us-east-1.amazonaws.com/',
+        'base' : 'https://5y96pktw7j.execute-api.us-east-1.amazonaws.com/',
         'list': '/employee/list',
         'add': '/employee/create',
         'delete': '/employee/delete/',
@@ -13,16 +13,16 @@ _AWS = {
         'auth': '/employee/auth'
     },
     'customers': {
-        'base' : 'https://7z6lcegucj.execute-api.us-east-1.amazonaws.com/',
+        'base' : 'https://u3tad4wx2c.execute-api.us-east-1.amazonaws.com/',
         'list': '/customers/list',
         'add': '/customers/create',
         'delete': '/customers/',
         'update': '/customers/update/',
+        'onetimes3url': '/customers/onetimes3url',
         'get': '/customers/',
-        'auth': '/customers/auth',
-        'share': '/customers/share/'
+        'auth': '/customers/auth'
     }
 }
 
 class Config(object):
-    SECRET_KEY = os.environ.get('SECRET_KEY') or 'ba62343d-31e4-4cbe-957c-cbc1f0e30a14'
+    SECRET_KEY = os.environ.get('SECRET_KEY') or 'za62343d-31e4-4cbe-957c-cbc1f0e30a14'
